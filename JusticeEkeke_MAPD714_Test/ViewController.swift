f6594c1dadb5ecc182b0491e9ec83b3e19dd878b
Justice Ekeke <justiceekeke1@gmail.com>
2021-10-30 15:35:46 -0400

    Initial Commit

---
+++ JusticeEkeke_MAPD714_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JusticeEkeke_MAPD714_Test
+//
+//  Created by Justice Ekeke on 30/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
